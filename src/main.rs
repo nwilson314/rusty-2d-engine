7d7514c4bcefba1e0d7e834666d1fb1b6e9abdea
nwilson314 <nsw.wilson@gmail.com>
2024-11-17 13:39:53 -0500

    reverting to < 0.3 version of winit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use anyhow::Result;
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::wasm_bindgen;
-
 use rusty_2d_engine::prelude::run;
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(main))]
-fn main() -> Result<()> {
+fn main() {
     run()
 }
